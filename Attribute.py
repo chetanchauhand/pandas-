58532c163e10d8507c37a199dbe6c0a2fe448f08
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-07 23:01:09 +0530

    inital

--- Attribute.py
+++ Attribute.py
@@ -1,10 +1,12 @@
 # Pandas dtype,ndim,size,name attribute
+import numpy as np
 import pandas as pd
 
-data = [10,20,30,40,50]
+# data = [10,20,30,40,50]
+data = [10,20,30,40,50,60,70,80,90,np.nan]
 
-# s = pd.Series(data)
-s = pd.Series(data ,name="NumberSeries")
+s = pd.Series(data)
+# s = pd.Series(data ,name="NumberSeries")
 
 print("Series: \n",s) # series
 
@@ -12,4 +14,5 @@ print("Series datatype: \n",s.dtype)
 print("Series dimension: \n",s.ndim)
 print("Series size:",s.size)
 print("Series name: ",s.name)
+print("Does the series has nan: \n",s.hasnans)
 
