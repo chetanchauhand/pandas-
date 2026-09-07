ff78dcdb0ef1e0970c2281c436ed1f41ea63e75b
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-07 22:55:16 +0530

    6th

--- Attribute.py
+++ Attribute.py
@@ -1,10 +1,15 @@
-# Pandas dtype attribute
+# Pandas dtype,ndim,size,name attribute
 import pandas as pd
 
 data = [10,20,30,40,50]
 
-s = pd.Series(data)
+# s = pd.Series(data)
+s = pd.Series(data ,name="NumberSeries")
 
 print("Series: \n",s) # series
 
 print("Series datatype: \n",s.dtype)
+print("Series dimension: \n",s.ndim)
+print("Series size:",s.size)
+print("Series name: ",s.name)
+
