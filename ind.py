f6ad978172279f2eb19bdc2c1fffba74ec0fb3d6
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-07 22:35:44 +0530

    4th

--- ind.py
+++ ind.py
@@ -1,8 +1,11 @@
-# Name your index in pandas series.
+# Name your index in pandas series and access value from a panda series with labels.
+ 
 
 import pandas as pd
 
 data = [10,20,30,40,50]
 
 s = pd.Series(data , index = ["Row1","Row2","Row3","Row4","Row5"])
-print("Series with data: ",s)
+print("Series with data: ",s) 
+
+print("Value from a pandas series with label : \n",s["Row3"])
