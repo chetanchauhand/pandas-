742ecd0c5b610d3bd7d18df7235b48b1601ecc0f
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-08 22:27:15 +0530

    1st

--- Cate.py
+++ Cate.py
@@ -1,6 +1,16 @@
-# Catogarical data in pandas.
+# Categorical data in pandas.
+
+# import pandas as pd
+
+# s = pd.Series(["a","b","c","d","e","f"],dtype = "category")
+# print(s)
+
+#Categorical dataframe in pandas.
 
 import pandas as pd
 
-s = pd.Series(["a","b","c","d","e","f"],dtype = "category")
+s = pd.DataFrame({"Cat1":list(),"Cate1":list(""),"Cat1":list(""),"Cat1":("")},dtype="category")
+
 print(s)
+
+print(s.dtypes)
