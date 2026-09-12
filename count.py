d943c1f90aab55f0ce924a85e5c23c990c1c8e79
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 23:11:47 +0530

    contain

--- count.py
+++ count.py
@@ -1,12 +1,18 @@
 #Count()
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 
-data = [np.nan,"Amit","Aman","Amar",np.nan]
+# data = [np.nan,"Amit","Aman","Amar",np.nan]
+
+data = ["Amit","Aman","Amar","Raj Malhotra"]
+
 
 s = pd.Series(data)
 print("Data series is:",s)
 
-rs = s.count()
+# rs = s.count()
+# print("\n Count value of series is:\n",rs)
+
+rs = s.__contains__('Raj')
 print("\n Count value of series is:\n",rs)
