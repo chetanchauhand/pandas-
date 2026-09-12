5086b18a008131ffe6e3fe954a2e65e6dd03bc54
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 15:34:00 +0530

    1st

--- colu.py
+++ colu.py
@@ -1,8 +1,26 @@
-#Select two column in pandas dataframe.
+# #Select two column in pandas dataframe.
+
+# import pandas as pd
+
+# data = {
+#      'Students' : ["Rahul","Mayank","Raghav","Manav","Jagan"],
+#      'Renk' : [1,2,5,4,3],
+#      'Marks' : [90,89,76,80,82]
+# }
+
+# res = pd.DataFrame(data)
+
+# print("Studnt record  : ",res)
+# print("Select only two column :",res[['Renk','Marks']])
+
+# # Selct multiple column in pandas dataframe.
 
 import pandas as pd
 
 data = {
+     'Roll No' : [100,101,102,103,104],
+     'Address' : ["Khanna","Doraha","Samarala","Dehru","Mohanpur"],
+     'Id' : [11,12,13,14,15],
      'Students' : ["Rahul","Mayank","Raghav","Manav","Jagan"],
      'Renk' : [1,2,5,4,3],
      'Marks' : [90,89,76,80,82]
@@ -11,4 +29,5 @@ data = {
 res = pd.DataFrame(data)
 
 print("Studnt record  : ",res)
-print("Select only two column :",res[['Renk','Marks']])
+print("Select only two column :",res[res.columns[3:5]])
+
