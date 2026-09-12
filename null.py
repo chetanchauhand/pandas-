1dfdb00e2c68c651e10c8a72ddec126d4c69cf8e
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 22:48:29 +0530

    au

--- null.py
+++ null.py
@@ -1,13 +1,14 @@
-# using isnull() method in pandas.
+# using isnull() and notnull() and dropna() method in pandas.
 
 import pandas as pd
 
 data = pd.read_csv("C:\\Users\\cheta\\Desktop\\Students.csv")
 
-print("Dataframe is :",data)
+print("Dataframe is :\n",data)
 
 # rs = data.isnull()
-rs = data.notnull()
+# rs = data.notnull()
+rs = data.dropna()
 
 
-print("Method in pandas :\n",rs)
+print("Method in pandas :\n",rs.to_string())
