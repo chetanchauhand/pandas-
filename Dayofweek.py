adec3ceeac8b6db3bd2c471dcf153efe4fee2cd5
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 23:29:29 +0530

    ini

--- Dayofweek.py
+++ Dayofweek.py
@@ -5,3 +5,13 @@ ts = pd.Timestamp(year=2026,month=9,day=12,hour=11)
 print("Date of time:\n",ts)
 
 print("\n Day of week:\n",ts.dayofweek)
+
+print("\n Day of week:\n",ts.dayofyear)
+
+print("\n Day of week:\n",ts.daysinmonth)
+
+print("\n Day of week:\n",ts.is_leap_year)
+
+print("\n Day of week:\n",ts.is_month_end)
+
+print("\n Day of week:\n",ts.is_month_start)
