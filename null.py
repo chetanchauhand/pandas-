7f20ec462a96bc1d3674e553c0a81047efeccaa1
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 22:44:45 +0530

    1st

--- null.py
+++ null.py
@@ -6,6 +6,8 @@ data = pd.read_csv("C:\\Users\\cheta\\Desktop\\Students.csv")
 
 print("Dataframe is :",data)
 
-rs = data.isnull()
+# rs = data.isnull()
+rs = data.notnull()
+
 
 print("Method in pandas :\n",rs)
