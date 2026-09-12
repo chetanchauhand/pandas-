017ac41a353a9d760a0face807e56471b6ec5503
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 16:18:05 +0530

    ini

---
+++ Iter.py
@@ -0,0 +1,17 @@
+# Iterate using iterrows(),itertuples() in pandas
+
+import pandas as pd
+
+data = {
+    'Id' : [11,12,13,14,15],
+         'Students' : ["Rahul","Mayank","Raghav","Manav","Jagan"],
+         'Renk' : [1,2,5,4,3]
+}
+
+res = pd.DataFrame(data)
+
+print("\n Students records :",res)
+
+print("\n Iterating Rows:")
+for i in res.iterrows():
+    print(i)
