c533ea1774a6ddbcd6f663f742259582646dc651
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 16:01:11 +0530

    ini

---
+++ DropCol.py
@@ -0,0 +1,19 @@
+# Drop column in pandas.
+
+import pandas as pd
+
+data = {
+     'Roll No' : [100,101,102,103,104],
+     'Address' : ["Khanna","Doraha","Samarala","Dehru","Mohanpur"],
+     'Id' : [11,12,13,14,15],
+     'Students' : ["Rahul","Mayank","Raghav","Manav","Jagan"],
+     'Renk' : [1,2,5,4,3],
+     'Marks' : [90,89,76,80,82]
+}
+
+res = pd.DataFrame(data)
+
+print("Studnt record  : ",res)
+
+resRf = res.drop("Id",axis='columns')
+print("Removed column is: ",resRf)
