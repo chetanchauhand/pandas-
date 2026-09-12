5746724f1f8d1a79c5adc4a60ffaadae5597af65
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 16:07:14 +0530

    ini

--- DropCol.py
+++ DropCol.py
@@ -15,5 +15,6 @@ res = pd.DataFrame(data)
 
 print("Studnt record  : ",res)
 
-resRf = res.drop("Id",axis='columns')
+# resRf = res.drop("Id",axis='columns')
+resRf = res.drop(res.columns[3], axis=1)
 print("Removed column is: ",resRf)
