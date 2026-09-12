55532e13719ef9a57f2b7157d7266ab0e952def9
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-12 23:01:50 +0530

    len

--- low.py
+++ low.py
@@ -10,6 +10,7 @@ print("\n Series of Student is : \n",series)
 
 # rs = series.str.lower()
 # rs = series.str.upper()
-rs = series.str.title()
+# rs = series.str.title()
+rs = series.str.len()
 
 print("\n lower of this series:\n",rs)
